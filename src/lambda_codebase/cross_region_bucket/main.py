aea94832be75318dc7bbc8d85559ab828dc74a07
Bridle <sbbridle@88e9fe72ca44.ant.amazon.com>
2020-06-22 11:13:05 +1000

    UpdatetoBucket

--- src/lambda_codebase/cross_region_bucket/main.py
+++ src/lambda_codebase/cross_region_bucket/main.py
@@ -144,7 +144,7 @@ def ensure_bucket(region: str, bucket_name_prefix: str) -> Tuple[BucketName, Cre
         bucket_name = "{0}-{1}".format(bucket_name_prefix, bucket_name_suffix)
         try:
             config = {'Bucket': bucket_name}
-            if region != 'ap-southeast-2':
+            if region != 'ua-east-1':
                 config["CreateBucketConfiguration"] = {"LocationConstraint": region}
             s3_client.create_bucket(
                 **config
